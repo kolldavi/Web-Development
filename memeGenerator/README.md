b5065ffbbcbe5a20f15e52a9528c975beb0b8a53
kolldavi <kolldavi@gmail.com>
2016-12-06 07:00:18 -0600

    added memeGenerator

---
+++ memeGenerator/README.md
@@ -0,0 +1,10 @@
+codecademy Meme Generator project
+=================================
+
+
+## Screenshot
+![screenshot](https://github.com/kolldavi/Web-Development/blob/master/memeGenerator/ScreenShotMemeGenerator.png?raw=true)
+
+
+Create by David Koller
+=======================
---
+++ memeGenerator/app.js
@@ -0,0 +1,14 @@
+var main = function() {
+  $('#top-text').keyup (function() {
+    $('.top-caption').text($(this).val());
+       });
+   $('#bottom-text').keyup (function() {
+    $('.bottom-caption').text($(this).val());
+       });
+   $('#image-url').keyup (function() {
+    $('.meme').children().attr("src",$(this).val());
+       });
+
+};
+
+$(document).ready(main);
