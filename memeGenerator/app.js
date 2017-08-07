052b2e49c2718ee22257dfcc2f87f706d8eb6bce
kolldavi <kolldavi@gmail.com>
2017-08-07 08:23:04 -0500

    format file

--- memeGenerator/app.js
+++ memeGenerator/app.js
@@ -2,24 +2,22 @@ var main = function() {
 
 
 
-  $('.toggle').on('click',function(){
+  $('.toggle').on('click', function() {
     $('.image-url').toggle();
   });
 
 
-  $('#top-text').on('input propertychange paste', function(){
+  $('#top-text').on('input propertychange paste', function() {
     $('.top-caption').text($(this).val());
-       });
-    $('#bottom-text').on('input propertychange paste', function(){
-          $('.bottom-caption').text($(this).val());
-       });
+  });
+  $('#bottom-text').on('input propertychange paste', function() {
+    $('.bottom-caption').text($(this).val());
+  });
 
 
-      $('#image-url').on('input propertychange paste', function(){
-          $('.meme').children().attr("src",$(this).val());
-      });
-
-
+  $('#image-url').on('input propertychange paste', function() {
+    $('.meme').children().attr("src", $(this).val());
+  });
 
 };
 
@@ -29,30 +27,31 @@ function update(jscolor) {
   document.getElementById('top').style.color = '#' + jscolor
   document.getElementById('bottom').style.color = '#' + jscolor
 }
- function handleFileSelect(evt) {
-var files = evt.target.files; // FileList object
 
-// Loop through the FileList and render image files as thumbnails.
-for (var i = 0, f; f = files[i]; i++) {
+function handleFileSelect(evt) {
+  var files = evt.target.files; // FileList object
 
-  // Only process image files.
-  if (!f.type.match('image.*')) {
-    continue;
+  // Loop through the FileList and render image files as thumbnails.
+  for (var i = 0, f; f = files[i]; i++) {
+
+    // Only process image files.
+    if (!f.type.match('image.*')) {
+      continue;
+    }
+
+    var reader = new FileReader();
+
+    // Closure to capture the file information.
+    reader.onload = (function(theFile) {
+      return function(e) {
+        $('.meme').children().attr("src", e.target.result);
+      };
+    })(f);
+
+    // Read in the image file as a data URL.
+    reader.readAsDataURL(f);
   }
-
-  var reader = new FileReader();
-
-  // Closure to capture the file information.
-  reader.onload = (function(theFile) {
-    return function(e) {
-    $('.meme').children().attr("src",e.target.result);
-    };
-  })(f);
-
-  // Read in the image file as a data URL.
-  reader.readAsDataURL(f);
 }
-}
-  document.getElementById('image-file').addEventListener('change', handleFileSelect, false);
+document.getElementById('image-file').addEventListener('change', handleFileSelect, false);
 
 $(document).ready(main);
